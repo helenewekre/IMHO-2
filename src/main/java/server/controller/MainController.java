dfc5fff7e9e3bc3a9c5e0ac345cda1548eac6394
Andy Tran <at@andys-mbp.hhk.dk>
2017-10-17 17:02:59 +0200

    Final merge of Gruppe1 branch.

--- src/main/java/server/controller/MainController.java
+++ src/main/java/server/controller/MainController.java
@@ -1,11 +1,40 @@
 package server.controller;
 
+import server.models.User;
+import server.dbmanager.dbmanager1;
+
+import java.util.Scanner;
+
+
 public class MainController {
     private AdminController adminController;
     private UserController userController;
+    private User currentUser;
+    private Scanner input;
+
 
     public MainController() {
 
     }
+
+    public boolean authUser(String username, String password) {
+        User user = new User();
+        dbmanager1 dbmanager1 = new dbmanager1();
+
+        user = dbmanager1.authorizeUser(username, password);
+
+        if(user!=null) {
+            user = currentUser;
+            return true;
+
+        }
+        else {
+            return false;
+        }
+
+    }
+
+
+
 
 }
--- src/main/java/server/endpoints/UserEndpoint.java
+++ src/main/java/server/endpoints/UserEndpoint.java
@@ -1,16 +1,28 @@
 package server.endpoints;
 
 import com.google.gson.Gson;
+import server.dbmanager.dbmanager1;
 import server.models.User;
+import server.utility.Digester;
 
 import javax.ws.rs.*;
 import javax.ws.rs.core.Response;
 import java.util.ArrayList;
 
 @Path("/user")
-
-
 public class UserEndpoint {
+
+    dbmanager1 dbmanager1 = new dbmanager1();
+
+    @POST
+    @Path("{username}/{password}")
+    public Response authorizeUser(@PathParam("username") String username, @PathParam("password") String password) {
+        User userFound = dbmanager1.authorizeUser(username, password);
+        return Response.status(200).entity(new Gson().toJson(userFound)).build();
+
+    }
+    //@POST
+    // public Response createUser(user)
 
     @GET
     public Response get() {
