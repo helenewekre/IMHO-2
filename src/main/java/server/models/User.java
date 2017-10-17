b991dbdf737640a2a7f0c2fae9e525ff94b7aaf3
Andy Tran <at@andys-mbp.hhk.dk>
2017-10-17 16:51:51 +0200

    Adds the models.

--- src/main/java/server/models/User.java
+++ src/main/java/server/models/User.java
@@ -1,33 +1,54 @@
 package server.models;
 
-
 public class User {
 
-    private int id;
-    private String name;
-    private int age;
+    private int idUser;
+    private int type;
+    private String username;
+    private String password;
 
-    public int getId() {
-        return id;
+    public User(String username, String password) {
+        this.idUser = idUser;
+        this.type = type;
+        this.username = username;
+        this.password = password;
     }
 
-    public void setId(int id) {
-        this.id = id;
+    public User() {
+
     }
 
-    public String getName() {
-        return name;
+    public int getIdUser() {
+        return idUser;
     }
 
-    public void setName(String name) {
-        this.name = name;
+    public void setIdUser(int idUser) {
+        this.idUser = idUser;
     }
 
-    public int getAge() {
-        return age;
+    public int getType() {
+        return type;
     }
 
-    public void setAge(int age) {
-        this.age = age;
+    public void setType(int type) {
+        this.type = type;
     }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    // Test!
 }
