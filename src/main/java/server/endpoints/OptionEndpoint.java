d55f9b3ab33ae73a4cb85fa2fbb99ef94a73d375
helenewekre <hewe16@student.cbs.dk>
2017-10-19 20:15:55 +0200

    optionendpoint

--- src/main/java/server/endpoints/OptionEndpoint.java
+++ src/main/java/server/endpoints/OptionEndpoint.java
@@ -2,6 +2,7 @@ package server.endpoints;
 
 import com.google.gson.Gson;
 import server.dbmanager.DbManager;
+import server.models.Option;
 import server.models.Question;
 
 import javax.ws.rs.GET;
@@ -13,6 +14,37 @@ import java.util.ArrayList;
 @Path("/option")
 public class OptionEndpoint {
 
+package server.endpoints;
+
+import com.google.gson.Gson;
+import server.controller.Config;
+import server.dbmanager.DbManager;
+import server.models.Option;
+import server.models.Quiz;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.core.Response;
+import java.util.ArrayList;
+
+    @Path("/option")
+    public class OptionEndpoint {
+
+        DbManager dbmanager = new DbManager();
+
+        @GET
+        @Path("/{question_id}")
+        public Response loadOptions (@PathParam("question_id") int questionId){
+
+                ArrayList<Option> options = dbmanager.loadOptions(questionId);
+                return Response.status(200).entity(new Gson().toJson(options)).build();
+
+
+            }
+
+        }
+    }
 
 
 }
