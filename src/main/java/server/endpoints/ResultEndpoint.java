72bc4d4effcb279c768f6e9221ffde3a1d0b7db1
Andy Tran <at@Andys-MacBook-Pro.local>
2017-10-22 22:25:42 +0200

    Token for resultendpoint

--- src/main/java/server/endpoints/ResultEndpoint.java
+++ src/main/java/server/endpoints/ResultEndpoint.java
@@ -1,29 +1,53 @@
 package server.endpoints;
 
 import com.google.gson.Gson;
+import server.controller.MainController;
 import server.controller.UserController;
 import server.models.Result;
+import server.utility.CurrentUserContext;
 
 import javax.ws.rs.GET;
+import javax.ws.rs.HeaderParam;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.core.Response;
+import java.sql.SQLException;
 
 @Path("/result")
 public class ResultEndpoint {
     UserController userController = new UserController();
+    MainController mainController = new MainController();
 
     @GET
     //send totale number of correct answer and questions back to the user.
     @Path("{quizID}/{userID}")
-    public Response getUserScore(@PathParam("quizID") int quizID, @PathParam("userID") int userID){
-        Result result = userController.getResult(quizID, userID);
+    public Response getUserScore(@HeaderParam("authorization") String token, @PathParam("quizID") int quizID, @PathParam("userID") int userID) throws SQLException {
+        CurrentUserContext context = mainController.getUserFromTokens(token);
 
-        return Response
-                .status(200)
-                .type("application/json")
-                .entity(new Gson().toJson(result))
-                .build();
+        if (context.getCurrentUser() != null) {
+            if(!context.isAdmin()) {
+                Result result = userController.getResult(quizID, userID);
+                return Response
+                        .status(200)
+                        .type("application/json")
+                        .entity(new Gson().toJson(result))
+                        .build();
+            } else {
+                return Response
+                        .status(200)
+                        .type("application/json")
+                        .entity("You are not authorized")
+                        .build();
+            }
+
+        } else {
+            return Response
+                    .status(200)
+                    .type("application/json")
+                    .entity("Error loading profile")
+                    .build();
+        }
+
 
     }
 }
