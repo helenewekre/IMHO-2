4c36791886874cca624c6e2ab4c8b781f30a60ee
Andy Tran <at@Andys-MacBook-Pro.local>
2017-10-31 13:54:42 +0100

    fix

--- src/main/java/server/controller/MainController.java
+++ src/main/java/server/controller/MainController.java
@@ -29,23 +29,26 @@ public class MainController {
     // Logic behind authorizing user
     public User authUser(User user) {
         String token = null;
+        //Use the username to get the time created
         User foundUser = dbManager.getTimeCreatedByUsername(user.getUsername());
+        //Add the time created to the password and hash
         user.setPassword(digester.hashWithSalt(user.getPassword() + foundUser.getTimeCreated()));
+        //Authorize the user
         User authorizedUser = dbManager.authorizeUser(user.getUsername(), user.getPassword());
 
+        //Generate token for login
         try {
             Algorithm algorithm = Algorithm.HMAC256("Secret");
             long timeValue = (System.currentTimeMillis() * 1000) + 20000205238L;
             Date expDate = new Date(timeValue);
 
             token = JWT.create().withClaim("User", authorizedUser.getUsername()).withExpiresAt(expDate).withIssuer("IMHO").sign(algorithm);
+            //Add the token to database
             dbManager.addToken(token, authorizedUser.getUserId());
-
         } catch (UnsupportedEncodingException e) {
             e.printStackTrace();
         }
         if (token != null) {
-            Globals.log.writeLog(getClass().getName(), this, "Auth. user", 2);
             return authorizedUser;
         } else {
             return null;
@@ -53,10 +56,8 @@ public class MainController {
     }
 
     //Logic behind creating user.
-
     public User createUser(User user) {
         long unixTime = (long) Math.floor(System.currentTimeMillis() / 10000);
-
         user.setTimeCreated(unixTime);
         user.setPassword(digester.hashWithSalt(user.getPassword()+user.getTimeCreated()));
 
