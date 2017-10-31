5f09dbcb873256d658bf3218acd7ecb0dcba92d5
Andy Tran <at@Andys-MacBook-Pro.local>
2017-10-31 14:02:47 +0100

    Fixed quizController

--- src/main/java/server/controller/QuizController.java
+++ src/main/java/server/controller/QuizController.java
@@ -18,6 +18,7 @@ public class QuizController {
         dbManager = new DbManager();
     }
 
+    //Method for loading courses
     public ArrayList<Course> loadCourses() {
         ArrayList<Course> courses = dbManager.loadCourses();
         return courses;
@@ -33,7 +34,6 @@ public class QuizController {
     public Quiz createQuiz(Quiz quiz) {
         Quiz createdQuiz = dbManager.createQuiz(quiz);
         if (createdQuiz != null) {
-            Globals.log.writeLog(getClass().getName(), this, "Quiz created", 2);
             return createdQuiz;
         } else {
             return null;
@@ -43,12 +43,7 @@ public class QuizController {
     //Method for deleting a quiz and all it's sub-tables
     public Boolean deleteQuiz(int quizId) {
         Boolean ifDeleted = dbManager.deleteQuiz(quizId);
-
-        if (ifDeleted) {
-            return true;
-        } else {
-            return false;
-        }
+        return ifDeleted;
     }
 
     public ArrayList<Question> loadQuestions(int quizId) {
@@ -59,8 +54,6 @@ public class QuizController {
     //Method for creating a question
     public Question createQuestion(Question question) {
         Question createdQuestion = dbManager.createQuestion(question);
-        Globals.log.writeLog(getClass().getName(), this, "Question created", 2);
-
         if (createdQuestion != null) {
             return createdQuestion;
         } else {
@@ -71,8 +64,6 @@ public class QuizController {
     //Method for creating a option
     public Option createOption(Option option) {
         Option newOption = dbManager.createOption(option);
-        Globals.log.writeLog(getClass().getName(), this, "Option created", 2);
-
         if (newOption != null) {
             return newOption;
         } else {
@@ -80,10 +71,9 @@ public class QuizController {
         }
 
     }
-
+    //Method for loading options
     public ArrayList<Option> getOptions(int idQuestion) {
         ArrayList<Option> options = dbManager.loadOptions(idQuestion);
-
         if(options !=null) {
             return options;
         } else {
