77921ab86229a6b0166a2348a9297f7cba09eaa5
helenewekre <hewe16@student.cbs.dk>
2017-12-21 20:32:44 +0100

    Deleted more unused code

--- src/main/java/server/endpoints/OptionEndpoint.java
+++ src/main/java/server/endpoints/OptionEndpoint.java
@@ -4,7 +4,6 @@ import com.google.gson.Gson;
 import server.controller.QuizController;
 import server.controller.MainController;
 import server.controller.TokenController;
-import server.controller.UserController;
 import server.models.Option;
 import server.utility.CurrentUserContext;
 import server.utility.Crypter;
@@ -19,7 +18,6 @@ import java.util.ArrayList;
 @Path("/option")
 public class OptionEndpoint {
     QuizController quizController = new QuizController();
-    UserController userController = new UserController();
     TokenController tokenController = new TokenController();
     Crypter crypter = new Crypter();
 
@@ -80,26 +78,4 @@ public class OptionEndpoint {
             return Response.status(500).type("text/plain").entity("Unauthorized").build();
         }
     }
-
-    @DELETE
-    @Path("{deleteId}")
-    public Response deleteAnswer(@HeaderParam("authorization") String token, @PathParam("deleteId") int userId) throws SQLException {
-        CurrentUserContext currentUser = tokenController.getUserFromTokens(token);
-
-        if (currentUser.getCurrentUser() != null) {
-            Boolean answerDeleted = userController.deleteAnswer(userId);
-            if (answerDeleted = true) {
-                Globals.log.writeLog(this.getClass().getName(), this, "Answer deleted", 2);
-                return Response.status(200).type("text/plain").entity("Answer deleted").build();
-            } else {
-                Globals.log.writeLog(this.getClass().getName(), this, "Delete answer attempt failed", 2);
-                return Response.status(400).type("text/plain").entity("Error deleting answer").build();
-            }
-        } else {
-            Globals.log.writeLog(this.getClass().getName(), this, "Unauthorized - delete answer", 2);
-            return Response.status(401).type("text/plain").entity("Unauthorized").build();
-        }
-    }
-
-
 }
